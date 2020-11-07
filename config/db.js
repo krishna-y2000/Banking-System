8ef14ea1fc41d841fc156934b4234441a0d2cb56
Krishnaraj Yadav <krishnay.75676@gmail.com>
2020-11-07 18:10:37 +0530

    heroku chnages 2

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config({path : './config.env'});
 
 const connectMongo = async() => {
     try{
-     await mongoose.connect(process.env.MONGODB_URI || process.env.MONGO_URI,{ useNewUrlParser: true , useUnifiedTopology: true, useCreateIndex : true } );
+     await mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true , useUnifiedTopology: true, useCreateIndex : true } );
      console.log(process.env.MONGO_URI);
      console.log("DB connected");
     }
