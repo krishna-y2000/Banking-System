e6604030a3ed065c5fcd96db61e9224f01204d4a
Krishnaraj Yadav <krishnay.75676@gmail.com>
2020-11-07 18:03:52 +0530

    changes to heroku

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 dotenv.config({path : './config.env'});
-console.log(process.env.MONGO_URI);
+
 const connectMongo = async() => {
     try{
-     await mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true , useUnifiedTopology: true, useCreateIndex : true } );
-    
+     await mongoose.connect(process.env.MONGODB_URI || process.env.MONGO_URI,{ useNewUrlParser: true , useUnifiedTopology: true, useCreateIndex : true } );
+     console.log(process.env.MONGO_URI);
      console.log("DB connected");
     }
     catch(e)
